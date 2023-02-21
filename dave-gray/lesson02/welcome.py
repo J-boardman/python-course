1d66e803419fb6202ef4aa10bec31bb332fa5a2e
Jayde Boardman <jaydeboardman@outlook.com>
2023-02-21 19:23:06 +1000

    Lesson 02 - pip & welcome.py

---
+++ dave-gray/lesson02/welcome.py
@@ -0,0 +1,5 @@
+line01 = "********************\n"  # header / footer
+line02 = "*                  *\n"  # re-use
+line03 = "*     WELCOME!     *\n"
+
+print('', line01, line02, line03, line02, line01)
